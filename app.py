c173f57320b4173408e4d0400dedc2eb3c946257
Alex Kamer <alexkamer82@gmail.com>
2025-01-22 14:54:27 -0600

    basicBoxscore

--- app.py
+++ app.py
@@ -32,11 +32,23 @@ def index():
     streams = get_live_streams(sport)
     return render_template('index.html', sports=sports, streams=streams, selected_sport=sport)
 
-@app.route('/stream', methods=['POST'])
+@app.route('/stream', methods=['GET', 'POST'])
 def stream():
     game_name = request.form['game_name']  # Retrieve the game name from the form submission
     game_url = request.form['game_url']
-    return render_template('stream.html', game_name=game_name, game_url=game_url)
+    selected_stream = request.form.get('stream_name', "1")  # Default to "Stream 1"
+
+    # Sample player boxscore data
+    player_boxscore = [
+        {"name": "Player 1", "points": 24, "rebounds": 10, "assists": 5},
+        {"name": "Player 2", "points": 18, "rebounds": 7, "assists": 8},
+        {"name": "Player 3", "points": 12, "rebounds": 5, "assists": 2},
+    ]
+
+
+    # return render_template('stream.html', game_name=game_name, game_url=game_url)
+    return render_template('stream.html', selected_stream=selected_stream, game_url=game_url, game_name=game_name, player_boxscore = player_boxscore)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
